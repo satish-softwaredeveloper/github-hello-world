685d13e1e9d14d74f41c08b14fac87d73a15dff7
satish-softwaredeveloper <73786285+satish-softwaredeveloper@users.noreply.github.com>
2021-02-04 16:32:05 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,16 @@
+# Addition fuctionality
+    def add(x,y):			
+	if x<y:
+      return ERROR
+	return x-y
+
+	
+# Substraction functionality
+def subtract(x,y):
+	pass
+
+def multiply(x,y):
+	return x*y
+	
+def divide(x,y):
+    return x/y;
